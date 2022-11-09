cb554e175bfea626b52f9973d4f73766709d22a4
MatiasUrielGluck <matiasugluck@gmail.com>
2022-11-09 08:25:35 -0300

    Solved bug when selecting random options

--- src/helpers/getOptions.js
+++ src/helpers/getOptions.js
@@ -1,16 +1,28 @@
+export const getOptionss = (countries, correctOption) => {
+  // Returns 3 options, different from the correct option which is passed as a parameter
+  const options = [];
+
+  for (let i = 0; i < 3; i++) {
+    let option = countries[0];
+
+    if (option.name !== correctOption.name && !options.includes(option)) {
+      options.push(option);
+    }
+  }
+
+  return options;
+};
+
 export const getOptions = (countries, correctOption) => {
   // Returns 3 options, different from the correct option which is passed as a parameter
   const options = [];
-  let counter = 0;
-
-  for (let i = 0; i < 3; i++) {
-    let option = countries[counter];
-
-    if (option.name !== correctOption.name) {
+  
+  while (options.length < 3) {
+    let option = countries[Math.floor(Math.random() * countries.length)];
+    
+    if (option.name !== correctOption.name && !options.includes(option)) {
       options.push(option);
     }
-
-    counter++;
   }
 
   return options;
